41a9815405078d3a1003c2bae0cd501265006d7a
Matt Sinwelski <Matt Sinwelski@LAPTOP-QBO3U58G>
2017-09-26 18:37:04 -0500

    starting lab 4

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,43 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+/**
+ * does.
+ * something
+ */
+public class ReclamationProject {
+    /**
+     * runs the program.
+     * If a and b are the same string then it prints the string minus the last letter.
+     * if one is shorter/longer than the other then it prints the equal characters
+     * @param args runs the program
+     */
+    public static void main(final String[] args) {
+        String a = "yessssssfgbsfgb";
+        String b = "yesssss";
+        String c = printMostInCommon(a, b);
+        System.out.println(c);
+    }
+    /**
+     *
+     * @param a you know i had to
+     * @param b do it to em
+     * @return returns something
+     */
+    public static String printMostInCommon(String a, String b) {
+        //if (a.length() > b.length()) {
+          //  String c = a;
+            //a = b;
+            //b = c;
+        //}
+        String r = "";
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    r = (a.regionMatches(i, b, k, j) && j > r.length()) ? a.substring(i, i + j) : r;
+                }
+            }
+        }
+        return r;
+    }
 }
